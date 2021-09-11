fafe28bf247c43545632af06c98f3ad854273661
Shakhzod Karimov <90506885+foydalideveloper@users.noreply.github.com>
2021-09-11 20:42:25 +0900

    Kelajak yoshni aniqlash
    if-else

---
+++ kelajakyoshhh.py
@@ -0,0 +1,11 @@
+# ism = input("Ismingizni kiriting: ")
+# yosh = int(input("Nechinchi yilda tug'ilgansiz': "))
+# data = 2021 - yosh
+# yil = int(input("Xozir nechinchi yil: "))
+# kelajak = int(input("Kelajakdagi biron yilni kiriting: "))
+# kelajak_yil = kelajak - yosh
+# if kelajak>2100:
+#     print(f"Kallang bormi {ism}? {kelajak} yilda san tirik bo'lasanmi yoqmi kim biladi! ")
+# else:
+#     print(f"Sizning ismingiz {ism} va siz xozir {data}da siz. Xozir {yil}yil va {kelajak}yilda siz {kelajak_yil}da bo'lasiz")
+
